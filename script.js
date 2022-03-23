e8e57079312a2ca71b17e99bcf33a872759aed9d
aaf-wtag <akib.fahad@welldev.io>
2022-03-23 12:50:57 +0600

    added initial functionalities

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+import { renderOnCreate, renderAllData } from "./render.js";
 
+// Select the create button element
+const create = document.querySelector('#create-button'); // returns null when importing Roboto from Google Fonts
+// console.log(create); 
+
+// Add a click event listener for the create button
+create.addEventListener('click', () => renderOnCreate());
+
+const all_button = document.querySelector("#all-button");
+all_button.addEventListener('click', event =>{
+    console.log("All button clicked!");
+});
+
+document.addEventListener('DOMContentLoaded', () => renderAllData());
